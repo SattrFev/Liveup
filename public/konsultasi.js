57ad922bf8bf7be7b34719307e43ce9e9330bec0
Ahmad-Yu2up-Ar-Raf <yusufzolldyck@gmail.com>
2025-01-08 09:35:45 +0700

    Menambahkan fitur baru untuk validasi form

--- public/konsultasi.js
+++ public/konsultasi.js
@@ -4,3 +4,24 @@
   function hahah(){
       console.log("hello world")
   }
+
+
+
+  
+  let swiper2 = new Swiper(".mySwiper2", {
+    grabCursor: true,
+    effect: "creative",
+    loop: true,
+    autoplay: true,
+ 
+    creativeEffect: {
+      prev: {
+        
+        translate: ["-200%", 10, -1000],
+      },
+      next: {
+        
+        translate: ["200%", 10, -1000],
+      },
+    },
+  });
